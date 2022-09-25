dbbba5d9abe635724d96a0b12cd6854dbc68a003
vidhihihi <114097650+vidhihihi@users.noreply.github.com>
2022-09-25 21:29:43 +0530

    Update class1.py

--- class1.py
+++ class1.py
@@ -1,13 +1,4 @@
-A1=["2","4","6"]
-A2=["5","10","15"]
-A2[1]=40
-print(A1,A2)
-A2.append(20)
-print(A2)
-A1.insert(2,1)
-print(A1)
-del A1[1]
-print(A1)
+
 Amish=["melluha","naags","vayuputras"]
 print(Amish)
 Jenny=["the summmer","always summer","its not summer"]
